4390c0500c40c81bd9cb384729f2995a2a3f16a7
Logan Gore <gorel@purdue.edu>
2014-01-29 15:14:30 -0500

    Added a liberal amount of comments.

--- src/Ride.java
+++ src/Ride.java
@@ -1,16 +1,41 @@
+/**
+  * File: Ride.java
+  * Last Modified: 1/29/2014
+  * @author Logan Gore
+  * This is a generic Ride class used to create RideRequests and RideOffers.
+  * Each ride has a departure time window, a start and end location, and a description field which may be empty.
+  * This class also has a method to determine if two dates overlap.
+  * If the closeWindow occurs before the openWindow, an IllegalArgumentException will be thrown.
+  * Note: This is an abstract class not meant to be instantiated -- create a RideOffer or RideRequest to specify the type of ride needed.
+  */
+import java.util.Date;
+
 public abstract class Ride
 {
 	protected Date departureOpen;		//The opening window of when the person offering the ride will be leaving
 	protected Date departureClose;		//The closing window of when the person offering the ride will be leaving
-	protected String description;		//A user's personal description if they wish to give one
+	protected String description;		//A user's personal description of the trip if they wish to give one
 	
-	//These are string objects for now.  We will replace with the appropriate Map API when we decide on one
+	//These are String objects for now.  We will replace with the appropriate Map API when we decide on one
 	//Note: Either startLocation or endLocation must be Purdue.
 	private String	startLocation;
 	private String	endLocation;
 	
-	public RideOffer(Date openWindow, Date closeWindow, String startLoc, String endLoc)
+	/**
+	  * Construct a RideOffer object with the given opening and closing time window for leaving,
+	  * the given start and end locations, and the optional description.
+	  * @param openWindow the earliest time that the user wishes to leave
+	  * @param closeWindow the latest time the user wishes to leave
+	  * @param startLoc the location the user wishes to leave from
+	  * @param endLoc the location the user wishes to arrive at
+	  * @param description an optional field of extra information the user wishes to list
+	  * @throws IllegalArgumentException if closeWindow < openWindow
+	  */
+	public RideOffer(Date openWindow, Date closeWindow, String startLoc, String endLoc, String description) throws IllegalArgumentException
 	{
+		if (closeWindow.compareTo(openWindow) < 1)
+			throw new IllegalArgumentException("Error: Ending departure time must occur after starting departure time.");
+		
 		this.departureOpen = openWindow;
 		this.departureClose = closeWindow;
 		this.startLocation = startLoc;
@@ -18,17 +43,32 @@ public abstract class Ride
 		this.description = "";
 	}
 	
+	//Getter methods
+	public Date getOpen()
+	{
+		return departureOpen;
+	}
+	
+	public Date getClose()
+	{
+		return departureClose;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	
 	/**
 	  * Determine whether or not the given Dates occur within the same window as this ride offer's.
 	  * If thisStart > otherEnd or thisEnd < otherStart, the dates do not overlap.
 	  * By de Morgan's law, the overlap test simplifies to thisStart <= otherEnd && thisEnd >= otherStart.
-	  * @param otherStart The start time of the Date to compare to
-	  * @param otherEnd The end time of the Date to compare to
+	  * @param otherStart The date we are comparing against
 	  * @return whether or not this ride's departure window overlaps with the given other date
 	  */
-	public boolean inRange(Date otherStart, Date otherEnd)
+	public boolean dateInRange(Ride other)
 	{
-		return (departureOpen.compareTo(otherEnd) < 1)
-				&& (otherStart.compareTo(departureClose) < 1);
+		return (departureOpen.compareTo(other.getClose()) < 1)
+				&& (other.getOpen().compareTo(departureClose) < 1);
 	}
 }
--- src/RideRequest.java
+++ src/RideRequest.java
@@ -1,12 +1,42 @@
+/**
+  * File: RideRequest.java
+  * Last Modified: 1/29/2014
+  * @author Logan Gore
+  * @see Ride
+  * This class represents a RideRequest that would be given by someone looking for a ride from other students.
+  * The constructor is almost identical to the abstract {@link Ride} class,
+  * but also includes an integer of how far away from the destination the user is willing to be dropped off.
+  * The student must specify a radius greater than or equal to zero, or an IllegalArgumentException will be thrown.
+  */
 import java.util.Date;
 
 public class RideRequest extends Ride
 {
-	protected int searchRadius;
-
-	public RideRequest(Date openWindow, Date closeWindow, String startLoc, String endLoc, int radius)
+	protected int searchRadius;		//How far away from the destination the user is willing to be dropped off
+	
+	/**
+	  * Construct a RideOffer object with the given opening and closing time window for leaving,
+	  * the given start and end locations, and the optional description.
+	  * @param openWindow The earliest time that the user wishes to leave
+	  * @param closeWindow The latest time the user wishes to leave
+	  * @param startLoc The location the user wishes to leave from
+	  * @param endLoc The location the user wishes to arrive at
+	  * @param description An optional field of extra information the user wishes to list
+	  * @param numSeats The number of seats the user is offering
+	  * @throws IllegalArgumentException if the user did not specify a valid number of open seats
+	  */
+	public RideRequest(Date openWindow, Date closeWindow, String startLoc, String endLoc, String description, int radius) throws IllegalArgumentException
 	{
+		if (radius < 0)
+			throw new IllegalArgumentException("Error: Radius must be greater than or equal to zero.");
+		
 		super(openWindow, closeWindow, startLoc, endLoc);
 		this.searchRadius = radius;
+	}
+	
+	//Getter methods
+	public int getSearchRadius()
+	{
+		return searchRadius;
 	}
 }
