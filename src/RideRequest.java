2519812a699fd011fe3105e1720e5ee522a79b2b
Logan Gore <gorel@purdue.edu>
2014-01-29 14:45:30 -0500

    Added searchRadius variable

--- src/RideRequest.java
+++ src/RideRequest.java
@@ -2,8 +2,11 @@ import java.util.Date;
 
 public class RideRequest extends Ride
 {
-	public RideRequest(Date openWindow, Date closeWindow, String startLoc, String endLoc)
+	protected int searchRadius;
+
+	public RideRequest(Date openWindow, Date closeWindow, String startLoc, String endLoc, int radius)
 	{
 		super(openWindow, closeWindow, startLoc, endLoc);
+		this.searchRadius = radius;
 	}
 }
