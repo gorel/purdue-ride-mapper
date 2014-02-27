ecdbac3027420bc108889bd2280694eec765a53f
sleewood <sleewood@purdue.edu>
2014-02-26 16:53:34 -0800

    Trying to debug server only bug with creating listings.

---
+++ www/test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+
+
+		<!-- Custom styles for this template -->
+		<link href="signin.css" rel="stylesheet">
+
+		<!-- Custom scripts for the datatimepicker -->
+		<link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css" />
+		<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>		
+		<script type="text/javascript" src="js/moment.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap-datetimepicker.min.js"></script>
+	</head>
+	<body>
+		<h1 align="center">Processing your request...</h1>
+		<h2 align="center">You will be redirected once your request has been processed.</h2>
+	</body>
+</html>
