e321d520dfdf9eaa6fdb3fe8a354191bb5db4691
Evan Arnold <arnold44@purdue.edu>
2014-03-02 05:54:37 -0500

    Added latitude/longitude lookup. Returns 0.0 for bad inputs (like a whole state, or gibberish)

--- www/modules/createListing/createListingProc.php
+++ www/modules/createListing/createListingProc.php
@@ -28,6 +28,19 @@
 			   $data = htmlspecialchars($data);
 			   return $data;
 			}
+
+			function qualityCode($qualityCode)
+			{
+				if(strpos($qualityCode,'C') !== false)//If the quality code contains 'C' it is bad
+				{
+					return false;
+				}
+				if(strpos($qualityCode,'P1') !== false || strpos($qualityCode,'A5') !== false)//If the quality code indicates an exact point or city, it's good
+				{
+					return true;
+				}
+				return false; //When in doubt, assume it's a bad code
+			}
 			
 			$startingAddress = $destinationAddress = $passengers = $dateTime = $isRequest = "";
 			
@@ -51,7 +64,47 @@
 			}
 			else
 			{				
-				//THIS IS WHERE YOU CALL THE MAPQUEST API
+				//THIS IS WHERE THE MAPQUEST API IS CALLED
+				//@author Evan Arnold <arnold4@purdue.edu>
+				//Sets the start/end lats and longs to their real-world values, and 0.0 if there is bad input (like just a state or gibberish)
+				//Start Location:
+				$mapquestResult = file_get_contents('http://www.mapquestapi.com/geocoding/v1/address?&key=Fmjtd%7Cluur210znh%2Cb0%3Do5-90ys0a&location=$startingAddress');
+				$parsedResult = json_decode($mapquestResult);
+				//Only parse result if the input address is good.
+				$addressQualityCode = $parsedResult->results->locations[0]->geocodeQualityCode;
+				if(qualityCode($addressQualityCode) === false)//If the quality code is bad
+				{
+					//GUYS WHAT DO WE DO IF IT IS BAD?
+					$startLatitude = 0.0;
+					$startLongitude = 0.0;
+				}				
+				else//If the quality of the input is good enough
+				{
+					$startLatitude = $parsedResult->results[0]->locations[0]->latLng->lat;
+					$startLongitude = $parsedResult->results[0]->locations[0]->latLng->lng;
+				}
+
+				//EndLocation
+				$mapquestResult2 = file_get_contents('http://www.mapquestapi.com/geocoding/v1/address?&key=Fmjtd%7Cluur210znh%2Cb0%3Do5-90ys0a&location=$destinationAddress');
+				$parsedResult2 = json_decode($mapquestResult2);
+				//Only parse result if the input address is good.
+				$addressQualityCode2 = $parsedResult2->results->locations[0]->geocodeQualityCode;
+				if(qualityCode($addressQualityCode2) === false)//If the quality code is bad
+				{
+					//GUYS WHAT DO WE DO IF IT IS BAD?
+					$endLatitude = 0.0;
+					$endLongitude = 0.0;
+				}				
+				else//If the quality of the input is good enough
+				{
+					$endLatitude = $parsedResult->results[0]->locations[0]->latLng->lat;
+					$endLongitude = $parsedResult->results[0]->locations[0]->latLng->lng;
+				}
+
+				//At this point the start and end Latitudes and Longitudes /should/ be correct.... if there was bad input they are 0.0. We need to handle this
+
+
+
 				$sql="INSERT INTO listings (startingAddress, endingAddress, isRequest, passengers, dateOfDeparture)
 				VALUES
 				('$startingAddress','$destinationAddress','$isRequest','$passengers','$dateTime')";
