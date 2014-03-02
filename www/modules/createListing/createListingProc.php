f611b9c737b916f9b3816e584fe94017e69eb5dd
Evan Arnold <arnold44@purdue.edu>
2014-03-02 16:19:59 -0500

    Re-instated error checking. My previous commit worked flawless

--- www/modules/createListing/createListingProc.php
+++ www/modules/createListing/createListingProc.php
@@ -73,37 +73,35 @@
 				$mapquestResult = file_get_contents("http://www.mapquestapi.com/geocoding/v1/address?&key=Fmjtd%7Cluur210znh%2Cb0%3Do5-90ys0a&location=" . $lookupStartingAddress ."");
 				$parsedResult = json_decode($mapquestResult);
 				//Only parse result if the input address is good.
-				//$addressQualityCode = $parsedResult->results->locations[0]->geocodeQualityCode;
-				//if(qualityCode($addressQualityCode) === false)//If the quality code is bad
-				//{
-				//	//GUYS WHAT DO WE DO IF IT IS BAD?
-				//	$startLatitude = 5.0;
-				//	$startLongitude = 5.0;
-				//}				
-				//else//If the quality of the input is good enough
-				//{
-				$startLatitude = $parsedResult->results[0]->locations[0]->latLng->lat;//Add dat tab
-				$startLongitude = $parsedResult->results[0]->locations[0]->latLng->lng;//Add dat tab
-				//}
+				$addressQualityCode = $parsedResult->results->locations[0]->geocodeQualityCode;
+				if(qualityCode($addressQualityCode) === false)//If the quality code is bad
+				{
+					//GUYS WHAT DO WE DO IF IT IS BAD?
+					$startLatitude = 5.0;
+					$startLongitude = 5.0;
+				}				
+				else//If the quality of the input is good enough
+				{
+					$startLatitude = $parsedResult->results[0]->locations[0]->latLng->lat;//Add dat tab
+					$startLongitude = $parsedResult->results[0]->locations[0]->latLng->lng;//Add dat tab
+				}
 
 				//EndLocation
 				$mapquestResult2 = file_get_contents("http://www.mapquestapi.com/geocoding/v1/address?&key=Fmjtd%7Cluur210znh%2Cb0%3Do5-90ys0a&location=" . $lookupDestinationAddress ."");
 				$parsedResult2 = json_decode($mapquestResult2);
 				//Only parse result if the input address is good.
-				//$addressQualityCode2 = $parsedResult2->results->locations[0]->geocodeQualityCode;
-				//if(qualityCode($addressQualityCode2) === false)//If the quality code is bad
-				//{
-				//	//GUYS WHAT DO WE DO IF IT IS BAD?
-				//	$endLatitude = 5.0;
-				//	$endLongitude = 5.0;
-				//}				
-				//else//If the quality of the input is good enough
-				//{
-				$endLatitude = $parsedResult2->results[0]->locations[0]->latLng->lat;//Add dat tab
-				$endLongitude = $parsedResult2->results[0]->locations[0]->latLng->lng;//Add dat tab
-				//}
-
-//				$endLocationString = $parsedResult2->results[0]->providedLocation->location;
+				$addressQualityCode2 = $parsedResult2->results->locations[0]->geocodeQualityCode;
+				if(qualityCode($addressQualityCode2) === false)//If the quality code is bad
+				{
+					//GUYS WHAT DO WE DO IF IT IS BAD?
+					$endLatitude = 5.0;
+					$endLongitude = 5.0;
+				}				
+				else//If the quality of the input is good enough
+				{
+					$endLatitude = $parsedResult2->results[0]->locations[0]->latLng->lat;//Add dat tab
+					$endLongitude = $parsedResult2->results[0]->locations[0]->latLng->lng;//Add dat tab
+				}
 
 				//At this point the start and end Latitudes and Longitudes /should/ be correct.... if there was bad input they are 0.0. We need to handle this.//This has been temporarily removed
 
