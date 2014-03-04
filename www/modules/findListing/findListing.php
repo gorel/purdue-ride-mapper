9d94a60df26de17812641ab9fba1d6efd806a578
m-isabel <m.isabellee@hotmail.com>
2014-03-04 02:01:55 -0500

    Restesting some stuff

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -19,7 +19,6 @@
 	</div>
 	<div class="col-lg-6">
 		<div id="map_canvas" style="height: 500px; width: 400px"></div>
-<<<<<<< HEAD
 	</div>
 </div>
 <?php
@@ -33,7 +32,7 @@
 	{
 		$sql = "SELECT * FROM listings";
 		$result = mysqli_query($con,$sql);
-		echo "<table border='1' align="left">
+		echo "<table border='1'>
 		<tr>
 		<th> StartingAddress </th>
 		<th> EndingAddress </th>
@@ -57,115 +56,5 @@
 	}
 	mysqli_close($con);
 ?>
-=======
->>>>>>> parent of d011902... Cleaning some stuff
-		<script>
-			//This script create the map with a default address.
-			//Its current location is somewhere by College Station
-			$(document).ready(function () 
-			{
-				var map = new GMaps
-				({
-					div: '#map_canvas',
-					lat: 40.463666,
-					lng: -86.945828,
-					position: 'BOTTOM_CENTER',
-					zoomControl : true,
-					zoomControlOpt: 
-					{
-						style : 'SMALL',
-					},
-					panControl : false,
-				});
-				
-				//This add the start address marker
-				map.addMarker
-				({
-					lat:40.463666,
-					lng: -86.945828,
-				});
-				
-				//This will add the route drawing from start to destination
-				//Colors can be changed (Its in Hex)
-				map.drawRoute
-				({
-					origin: [40.463666,-86.945828],
-					destination: [40.422906,-86.910637],
-					travelMode: 'driving',
-					strokeColor: '#0000FF',
-					strokeOpacity: 0.6,
-					strokeWeight: 6
-				});
-				
-				//This add the destination address marker
-				map.addMarker
-				({
-					lat:40.422906,
-					lng:-86.910637,
-				});
-				
-				map.addMarker
-				({
-					lat:40.602875,
-					lng:-86.874245,
-				});			
-				
-				map.addMarker
-				({
-					lat:39.772659,
-					lng:-86.167359,
-				});
-								
-				map.drawRoute
-				({
-					origin: [40.602875,-86.874245],
-					destination: [39.772659,-86.167359],
-					travelMode: 'driving',
-					strokeColor: '#E9BBC1',
-					strokeOpacity: 0.6,
-					strokeWeight: 6
-				});
-				
-				map.fitZoom();
-			});
-		</script>
-<<<<<<< HEAD
-=======
-	</div>
-</div>
-<?php
-	$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
-	
-	if(mysqli_connect_errno())
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-	else
-	{
-		$sql = "SELECT * FROM listings";
-		$result = mysqli_query($con,$sql);
-		echo "<table border='1'>
-		<tr>
-		<th> startingAddress </th>
-		<th> endingAddress </th>
-		</tr>";
-		while($row = mysqli_fetch_array($result))
-		{
-		echo "<tr>";
-		echo "<td>". $row['startingAddress'] . "</td>";
-		echo "<td>". $row["endingAddress"] . "</td>";
-		echo "</tr>";
-		/*
-			$req	=	$row["isRequest"];
-			$pass	=	$row["passengers"];
-			$dDept	=	$row["dateOfDeparture"];
-			$user	=	$row["user_id"];
-		*/
-		}
-		echo "</table>";
-	}
-	mysqli_close($con);
-?>
->>>>>>> parent of d011902... Cleaning some stuff
 </body>
 </html>
