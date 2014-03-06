154cc429065a18a9d5a122831fbc2dabdff6227f
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-03-06 17:23:22 -0500

    new error handling

---
+++ www/modules/signin/passwordError.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Password does not match E-Mail address";
+	echo "You will be redirected to the main page in 5 seconds.. if you still use IE6 and just want to watch the world burn, refresh the page yourself";
+	time.sleep(5);
+	header("Location:../../index.php");
+?>
--- www/modules/signin/signinProc.php
+++ www/modules/signin/signinProc.php
@@ -58,17 +58,11 @@ else
 	}
 	else if(!strcmp($password, $hashpw) && $verified==0)
 	{
-		echo "You are not a verified user";
-		echo "You will be redirected to main page in 5 seconds.. if your browser is IE6 and you just want to watch the world burn, refresh the webpage";
-		time.sleep(5);
-		header("Location:../../index.php");
+		header("Location:passwordError.php");
 	}
 	else
 	{
-		echo "Password is not correct for E-Mail address";
-		echo "You will be redirected to main page in 5 seconds.. if your browser is IE6 and you just want to watch the world burn, refresh the webpage";
-		time.sleep(5);
-		header("Location:../../index.php");
+		header("Location:verifiedError.php");
 	}
 }
 
