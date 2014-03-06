78a69482ebd45aa1e4161068cf07521cc5c7f89c
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-03-06 17:24:58 -0500

    bug fix

--- www/modules/signin/verifiedError.php
+++ www/modules/signin/verifiedError.php
@@ -1,6 +1,6 @@
 <?php
 	echo "Your account has not been verified";
 	echo "You will be redirected to the main page in 5 seconds.. if you still use IE6 and just want to watch the world burn, refresh the page yourself";
-	time.sleep(5);
+	sleep(5);
 	header("Location:../../index.php");
 ?>
