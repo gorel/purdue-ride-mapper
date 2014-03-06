5e9639815ace6e9146691269ee4ffbb029df1741
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-03-06 00:46:56 -0500

    fix

--- www/modules/signin/signoutProc.php
+++ www/modules/signin/signoutProc.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
 session_destroy();
-echo "<script type='text/javascript'>alert('logged out'); header(\"Location:../../index.php\");</script>";
+echo "<script type='text/javascript'>alert('logged out');</script>";
+header('Location: ../../index.php');
 ?>
