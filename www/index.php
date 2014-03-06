b15f88277ebc35df78c759b4759be61bcfa8cd74
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-03-05 21:55:41 -0500

    fix

--- www/index.php
+++ www/index.php
@@ -59,7 +59,7 @@ if (!isset($_SESSION['user']))
 else
 {
 	echo '<li><a href="#" id="listARide" onclick="hideAll(this);">Create a Ride</a></li>';
-	echo '<li><a href="#" id="login" onclick="hideAll(this)">Log Out</a></li>';
+	echo '<li><a href="#" id="login" onclick="session_destroy();">Log Out</a></li>';
 }
 ?>
 					</ul>
