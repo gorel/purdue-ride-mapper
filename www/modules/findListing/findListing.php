b28a637940baff817588ed5f6d3b14c0dacc5315
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-03-06 09:05:29 -0500

    Let's now show listing unless uesr is logged in.

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -15,13 +15,13 @@
 					<input type="text" class="form-control" placeholder="Destination Address">
 				</div>
 				<button type="submit" class="btn btn-default" onclick="calcRoute();" >Search</button>
-			</form>			
-		</div>		
+			</form>
+		</div>
 		<br>
 		<script>
 			//This script create the map with a default address.
 			//Its current location is somewhere by College Station
-			$(document).ready(function () 
+			$(document).ready(function ()
 			{
 				var map = new GMaps
 				({
@@ -29,20 +29,20 @@
 					lat: 40.463666,
 					lng: -86.945828,
 					zoomControl : true,
-					zoomControlOpt: 
+					zoomControlOpt:
 					{
 						style : 'SMALL',
 					},
 					panControl : false,
 				});
-				
+
 				//This add the start address marker
 				map.addMarker
 				({
 					lat:40.463666,
 					lng: -86.945828,
 				});
-				
+
 				//This will add the route drawing from start to destination
 				//Colors can be changed (Its in Hex)
 				map.drawRoute
@@ -54,26 +54,26 @@
 					strokeOpacity: 0.6,
 					strokeWeight: 6
 				});
-				
+
 				//This add the destination address marker
 				map.addMarker
 				({
 					lat:40.422906,
 					lng:-86.910637,
 				});
-				
+
 				map.addMarker
 				({
 					lat:40.602875,
 					lng:-86.874245,
-				});			
-				
+				});
+
 				map.addMarker
 				({
 					lat:39.772659,
 					lng:-86.167359,
 				});
-								
+
 				map.drawRoute
 				({
 					origin: [40.602875,-86.874245],
@@ -83,53 +83,61 @@
 					strokeOpacity: 0.6,
 					strokeWeight: 6
 				});
-				
+
 				map.fitZoom();
 			});
 		</script>
 	<div>
 	<?php
-		$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
-		
-		if(mysqli_connect_errno())
+		session_start();
+		if (!isset($_SESSION['user']))
 		{
-			echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		}
 		else
 		{
-			$sql = "SELECT * FROM listings";
-			$result = mysqli_query($con,$sql);
-			echo "<table class='table table-striped'>
-			<thead>
-			<tr>
-			<th> StartingAddress </th>
-			<th> EndingAddress </th>
-			<th> Request? </th>
-			<th> Passengers </th>
-			<th> Date of Departure </th>
-			</tr>
-			</thead>";
-			while($row = mysqli_fetch_array($result))
-			{
-				echo "<tr>";
-				echo "<td>". $row['startingAddress'] . "</td>";
-				echo "<td>". $row["endingAddress"] . "</td>";
-				if($row["isRequest"] == 0)
-				{
-					echo "<td> No </td>";					
-				}
-				else
-				{
-					echo "<td> Yes </td>";					
-				}
 
-				echo "<td>". $row["passengers"] . "</td>";
-				echo "<td>". $row["dateOfDeparture"] . "</td>";
-				echo "</tr>";
+			$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
+
+			if(mysqli_connect_errno())
+			{
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
 			}
-			echo "</table>";
+			else
+			{
+				$sql = "SELECT * FROM listings";
+				$result = mysqli_query($con,$sql);
+				echo "<table class='table table-striped'>
+				<thead>
+				<tr>
+				<th> StartingAddress </th>
+				<th> EndingAddress </th>
+				<th> Request? </th>
+				<th> Passengers </th>
+				<th> Date of Departure </th>
+				</tr>
+				</thead>";
+				while($row = mysqli_fetch_array($result))
+				{
+					echo "<tr>";
+					echo "<td>". $row['startingAddress'] . "</td>";
+					echo "<td>". $row["endingAddress"] . "</td>";
+					if($row["isRequest"] == 0)
+					{
+						echo "<td> No </td>";
+					}
+					else
+					{
+						echo "<td> Yes </td>";
+					}
+
+					echo "<td>". $row["passengers"] . "</td>";
+					echo "<td>". $row["dateOfDeparture"] . "</td>";
+					echo "</tr>";
+				}
+				echo "</table>";
+			}
+			mysqli_close($con);
 		}
-		mysqli_close($con);
 	?>
 	</div>
 </body>
