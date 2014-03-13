8bc3922cb9522e9a13f235274399cddd05afa789
sleewood <sleewood@purdue.edu>
2014-03-12 17:03:47 -0700

    displaying listings

--- www/modules/editListings/editListings.php
+++ www/modules/editListings/editListings.php
@@ -1,6 +1,48 @@
 <hr class="featurette-divider">
 <div class="container" >
 	<div class="row">
-		<p>Edit Listings</p>
+		<?php
+			$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
+
+			if(mysqli_connect_errno())
+			{
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+			else
+			{
+				$sql = "SELECT * FROM listings";
+				$result = mysqli_query($con,$sql);
+				echo "<table class='table table-striped'>
+				<thead>
+				<tr>
+				<th> StartingAddress </th>
+				<th> EndingAddress </th>
+				<th> Request? </th>
+				<th> Passengers </th>
+				<th> Date of Departure </th>
+				</tr>
+				</thead>";
+				while($row = mysqli_fetch_array($result))
+				{
+					echo "<tr>";
+					echo "<td>". $row['startingAddress'] . "</td>";
+					echo "<td>". $row["endingAddress"] . "</td>";
+					if($row["isRequest"] == 0)
+					{
+						echo "<td> No </td>";
+					}
+					else
+					{
+						echo "<td> Yes </td>";
+					}
+
+					echo "<td>". $row["passengers"] . "</td>";
+					echo "<td>". $row["dateOfDeparture"] . "</td>";
+					echo "</tr>";
+				}
+				echo "</table>";
+			}
+			mysqli_close($con);
+		?>
 	</div> <!-- row -->
 </div> <!-- /container -->
