e4f78db9d14768e9f2a55e9675afc8ae159e85bb
m-isabel <m.isabellee@hotmail.com>
2014-03-14 03:30:41 -0400

    ...maybe?

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -36,10 +36,10 @@
 					panControl : false,
 				});
 				var rides = "<?php echo $num_rides; ?>";
-				lat1 = "<?php echo json_encode($start_lat1); ?>";
-				lat2 = "<?php echo json_encode($start_lat2); ?>";
-				lon1 = "<?php echo json_encode($end_lon1); ?>";
-				lon2 = "<?php echo json_encode($end_lon2); ?>";
+				var lat1 = "<?php echo json_encode($start_lat1); ?>";
+				var lat2 = "<?php echo json_encode($start_lat2); ?>";
+				var lon1 = "<?php echo json_encode($end_lon1); ?>";
+				var lon2 = "<?php echo json_encode($end_lon2); ?>";
 				for (var i = 0; i < rides;i++)
 				{
 					//This add the start address marker
