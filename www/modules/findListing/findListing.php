8584d5eafbf22d5cebb8ad6df70983417fbb4758
Logan Gore <gorel@purdue.edu>
2014-03-15 20:16:58 -0400

    Debugging

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -88,7 +88,8 @@
 			}
 			else
 			{
-				$output = exec('python ../../../src/matcher.py 1')
+				//$output = exec('python ../../../src/matcher.py 1')
+				$output = "95 2\n75 4\n 34 5\n 13 8\n"
 				foreach(explode('\n', $output) as $line)
 				{
 					echo "<h1>$line</h1>"
