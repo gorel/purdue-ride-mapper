d37028f515fdb729b0cf54b274d891c0af1a0fc1
Logan Gore <gorel@purdue.edu>
2014-03-15 22:46:19 -0400

    Trying to allow the user to match a certain listing

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -17,8 +17,25 @@
 				<button type="submit" class="btn btn-default" onclick="calcRoute();" >Search</button>
 			</form>
 		</div>
+		
+		<div id="listing_id_input" style="height: 400px; width: 100%"></div>
+		<hr class="featurette-divider">
+		<div>
+			<h2 class="form-signin-heading">Match a ride:</h2>
+			<form class="form-inline" role="form">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Listing ID">
+				</div>
+				<button type="submit" class="btn btn-default" onclick="getMatches();" >Search</button>
+			</form>
+		</div>
 		<br>
 		<script>
+			function getMatches()
+			{
+				document.write('<h1>It works!</h1>');
+			}
+		
 			//This script create the map with a default address.
 			//Its current location is somewhere by College Station
 			$(document).ready(function ()
@@ -75,7 +92,8 @@
 	<div>
 	<?php
 		session_start();
-
+		
+		//Use this to print variables to the web console for debugging purposes
 		function debug_to_console($data)
 		{
 			if (is_array($data))
@@ -97,41 +115,56 @@
 				echo "Failed to connect to MySQL: " . mysqli_connect_error();
 			}
 			else
-			{
-				echo "<table class='table table-striped'>
-				<thead>
-				<tr>
-				<th> Match Percentage </th>
-				<th> Starting Address </th>
-				<th> Ending Address </th>
-				<th> Date of Departure </th>
-				</tr>
-				</thead>";
-				
+			{				
 				//Find matches to this listing
 				$output = exec('python ../../../src/matcher.py 1');
+				
+				if (0)
+				{
+				
+				}
+				else
+				{
+					echo "<table class='table table-striped'>
+					<thead>
+					<tr>
+					<th> Listing ID </th>
+					<th> Match Percentage </th>
+					<th> Starting Address </th>
+					<th> Ending Address </th>
+					<th> Date of Departure </th>
+					</tr>
+					</thead>";
+				}
 
 				//TODO: If len(output) == 0, print "no matches"
 
 				//For each match
 				foreach(explode('\n', $output) as $line)
 				{
+					//Get the match's score and id
 					$val = explode(' ', $line);
-					$sql = "SELECT * FROM listings WHERE listings_id=$val[1]";
-					debug_to_console($sql);
+					$score = $val[0]
+					$id = $val[1]
+					
+					//Get the match from the database
+					$sql = "SELECT * FROM listings WHERE listings_id=$id";
 					$result = mysqli_query($con,$sql);
+					
+					//Print the match to the table
 					while($row = mysqli_fetch_array($result))
 					{
 						echo "<tr>";
-						echo '<td>'. $val[0] .'</td>';
+						echo '<td>'. $id . '</td>';
+						echo '<td>'. $score .'</td>';
 						echo "<td>". $row['startingAddress'] . "</td>";
 						echo "<td>". $row["endingAddress"] . "</td>";
 						echo "<td>". $row["dateOfDeparture"] . "</td>";
-						echo "<td>". $i . "</td>";
 						echo "</tr>";
-						}
+					}
 				}
 			
+				//Print every listing in the database
 				$sql = "SELECT * FROM listings";
 				$result = mysqli_query($con,$sql);
 				echo "<table class='table table-striped'>
