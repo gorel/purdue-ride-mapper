05973a96ba455d294c9bf1461bb69d10f9d92d82
sleewood <sleewood@purdue.edu>
2014-03-25 01:13:01 -0400

    added delete sql statements

--- www/modules/editListings/editListingsProc.php
+++ www/modules/editListings/editListingsProc.php
@@ -1,3 +1,15 @@
 <?php
-echo '<script type="text/javascript">alert("hello!");</script>'; 
+	$listings_id = $_GET['listings_id'];
+	$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
+
+	if(mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	else
+	{
+		$sql = "DELETE FROM listings WHERE listings_id = $listings_id";
+		$result = mysqli_query($con,$sql);		
+	}
+	mysqli_close($con);
 ?>
