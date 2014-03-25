3b3e0879a3abc460194a8c076b83494059f5ad06
sleewood <sleewood@purdue.edu>
2014-03-24 21:18:54 -0400

    Added modal windows.

--- www/modules/editListings/editListings.php
+++ www/modules/editListings/editListings.php
@@ -14,78 +14,75 @@
 <script src="http://code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.js"></script>
 <hr class="featurette-divider">
 <div class="container" >
-<!-- Button trigger modal -->
-<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-  Launch demo modal
-</button>
+	<?php
+		$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
 
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-		<?php
-			$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
-
-			if(mysqli_connect_errno())
+		if(mysqli_connect_errno())
+		{
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+		else
+		{
+			session_start();
+			$user_id = $_SESSION['user'];
+		
+			$sql = "SELECT * FROM listings";
+			$result = mysqli_query($con,$sql);
+			echo "<table class='table table-striped'>
+			<thead>
+			<tr>
+			<th> StartingAddress </th>
+			<th> EndingAddress </th>
+			<th> Request? </th>
+			<th> Passengers </th>
+			<th> Date of Departure </th>
+			</tr>
+			</thead>";
+			while($row = mysqli_fetch_array($result))
 			{
-				echo "Failed to connect to MySQL: " . mysqli_connect_error();
-			}
-			else
-			{
-				session_start();
-				$user_id = $_SESSION['user'];
-			
-				$sql = "SELECT * FROM listings";
-				$result = mysqli_query($con,$sql);
-				echo "<table class='table table-striped'>
-				<thead>
-				<tr>
-				<th> StartingAddress </th>
-				<th> EndingAddress </th>
-				<th> Request? </th>
-				<th> Passengers </th>
-				<th> Date of Departure </th>
-				</tr>
-				</thead>";
-				while($row = mysqli_fetch_array($result))
+				if($_SESSION['user'] == $row["user_id"])
 				{
-					if($_SESSION['user'] == $row["user_id"])
+					echo "<tr>";
+					echo "<td>". $row['startingAddress'] . "</td>";
+					echo "<td>". $row["endingAddress"] . "</td>";
+					if($row["isRequest"] == 0)
 					{
-						echo "<tr>";
-						echo "<td>". $row['startingAddress'] . "</td>";
-						echo "<td>". $row["endingAddress"] . "</td>";
-						if($row["isRequest"] == 0)
-						{
-							echo "<td> No </td>";
-						}
-						else
-						{
-							echo "<td> Yes </td>";
-						}
-
-						echo "<td>". $row["passengers"] . "</td>";
-						echo "<td>". $row["dateOfDeparture"] . "</td>";
-						echo "<td> <button type=\"button\" class=\"btn btn-success\">Edit</button> </td>";
-						echo "<td> <button type=\"button\" class=\"btn btn-danger\">Delete</button> </td>";
-						echo "</tr>";
+						echo "<td> No </td>";
 					}
+					else
+					{
+						echo "<td> Yes </td>";
+					}
+
+					echo "<td>". $row["passengers"] . "</td>";
+					echo "<td>". $row["dateOfDeparture"] . "</td>";
+					echo "<td> <button class=\"btn btn-success\" data-toggle=\"modal\" data-target=\"#myModal\">Edit</button> </td>";
+					echo "<td> <button type=\"button\" class=\"btn btn-danger\">Delete</button> </td>";
+					echo "</tr>";
 				}
-				echo "</table>";
 			}
-			mysqli_close($con);
-		?>
+			echo "</table>";
+		}
+		mysqli_close($con);
+	?>
+	
+			<!-- Modal -->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			<h4 class="modal-title" id="myModalLabel">Modal title</h4>
+		  </div>
+		  <div class="modal-body">
+			...
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			<button type="button" class="btn btn-primary">Save changes</button>
+		  </div>
+		</div>
+	  </div>
+	</div>
+
 </div> <!-- /container -->
