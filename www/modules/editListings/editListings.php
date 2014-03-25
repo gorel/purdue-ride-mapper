32a999805f5b9979916ea788ed28060f3b280e54
sleewood <sleewood@purdue.edu>
2014-03-24 21:59:07 -0400

    Fixed php syntax error.

--- www/modules/editListings/editListings.php
+++ www/modules/editListings/editListings.php
@@ -56,8 +56,8 @@
 
 					echo "<td>". $row["passengers"] . "</td>";
 					echo "<td>". $row["dateOfDeparture"] . "</td>";
-					echo "<td> <button class=\"btn btn-success\" data-toggle=\"modal\" data-target=\"#myModal\" value="">Edit</button> </td>";
-					echo "<td> <button type=\"button\" class=\"btn btn-danger\" value="">Delete</button> </td>";
+					echo "<td> <button class=\"btn btn-success\" data-toggle=\"modal\" data-target=\"#myModal\">Edit</button> </td>";
+					echo "<td> <button type=\"button\" class=\"btn btn-danger\">Delete</button> </td>";
 					echo "</tr>";
 				}
 			}
