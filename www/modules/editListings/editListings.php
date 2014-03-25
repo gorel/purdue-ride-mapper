699d30c0c7f771650c0f2051644cb18a1af7e373
sleewood <sleewood@purdue.edu>
2014-03-25 00:33:16 -0400

    reverted

--- www/modules/editListings/editListings.php
+++ www/modules/editListings/editListings.php
@@ -1,10 +1,20 @@
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.css" rel="stylesheet">
+
+
+<!-- Custom styles for this template -->
+<link href="signin.css" rel="stylesheet">
+
+<!-- Custom scripts for the datatimepicker -->
+<link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css" />
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>		
 <script type="text/javascript" src="js/moment.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.js"></script>
 <hr class="featurette-divider">
 <div class="container" >
-	<?php		
+	<?php
 		$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
 
 		if(mysqli_connect_errno())
@@ -47,34 +57,32 @@
 					echo "<td>". $row["passengers"] . "</td>";
 					echo "<td>". $row["dateOfDeparture"] . "</td>";
 					echo "<td> <button class=\"btn btn-success\" data-toggle=\"modal\" data-target=\"#myModal\">Edit</button> </td>";
-					echo "<td> 
-					<button type=\"button\" class=\"btn btn-danger\">Delete</button> 
-					</td>";
+					echo "<td> <button type=\"button\" class=\"btn btn-danger\">Delete</button> </td>";
 					echo "</tr>";
 				}
 			}
 			echo "</table>";
 		}
 		mysqli_close($con);
+	?>
 	
-	
-		<!-- Modal -->
-		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		  <div class="modal-dialog">
-			<div class="modal-content">
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title" id="myModalLabel">Edit Listing</h4>
-			  </div>
-			  <div class="modal-body">
-				...
-			  </div>
-			  <div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				<button type="button" class="btn btn-primary">Save changes</button>
-			  </div>
-			</div>
+			<!-- Modal -->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			<h4 class="modal-title" id="myModalLabel">Edit Listing</h4>
+		  </div>
+		  <div class="modal-body">
+			...
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			<button type="button" class="btn btn-primary">Save changes</button>
 		  </div>
 		</div>
+	  </div>
+	</div>
 
 </div> <!-- /container -->
