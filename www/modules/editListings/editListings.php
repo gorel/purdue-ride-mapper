3e7195510cc82d30da58b5980d5d983957671ac5
sleewood <sleewood@purdue.edu>
2014-03-31 13:35:07 -0400

    fixed syntax, padding completed.

--- www/modules/editListings/editListings.php
+++ www/modules/editListings/editListings.php
@@ -86,8 +86,8 @@
 							<div class="form-group">
 								<div class = "col-lg-1" />
 								<div class = "col-lg-10">
-								<label>Starting Address</label>
-								<input type="text" class="form-control" placeholder="Starting Location" name="startingAddress" required autofocus>
+									<label>Starting Address</label>
+									<input type="text" class="form-control" placeholder="Starting Location" name="startingAddress" required autofocus>
 								</div>
 							</div>
 							   
@@ -95,37 +95,37 @@
 							<div class="form-group" id="test">
 								<div class = "col-lg-1" />
 								<div class = "col-lg-10">
-								<label>Ending Address</label>
-								<input type="text" class="form-control" placeholder="Destination" name="destinationAddress" required autofocus>
+									<label>Ending Address</label>
+									<input type="text" class="form-control" placeholder="Destination" name="destinationAddress" required autofocus>
 								</div>
 							</div>
 							
 							<div class="form-group">
 								<div class = "col-lg-1" />
 								<div class = "col-lg-10">
-								<input type="radio" name="isRequest" value="1" onClick="disablePassengers()" checked> Looking for a Ride 
-								<input type="radio" name="isRequest" value="0" onClick="enablePassengers()"> Hosting a Ride 			
+									<input type="radio" name="isRequest" value="1" onClick="disablePassengers()" checked> Looking for a Ride 
+									<input type="radio" name="isRequest" value="0" onClick="enablePassengers()"> Hosting a Ride 			
 								</div>
 							</div>
 							
 							<div class="form-group">
 								<div class = "col-lg-1" />
 								<div class = "col-lg-10">
-								<label>Number of Passengers</label>
-								<input id="passengersTextBox" type="text" class="form-control" placeholder="Number of Passengers" name="passengers" required autofocus disabled>
+									<label>Number of Passengers</label>
+									<input id="passengersTextBox" type="text" class="form-control" placeholder="Number of Passengers" name="passengers" required autofocus disabled>
 								</div>
 							</div>						
 							
 							<div class="form-group">
 								<div class = "col-lg-1" />
 								<div class = "col-lg-10">
-								<label>Date of Departure</label>
-								<div class='input-group date' id='datetimepicker1'>								
-									<input type='text' class="form-control" name="dateTime" placeholder="Desired Departure Date" data-format="YYYY-MM-DD hh:mm:ss"/>
-									<span class="input-group-addon">
-										<span class="glyphicon glyphicon-calendar"></span>
-									</span>
-								</div>
+									<label>Date of Departure</label>
+									<div class='input-group date' id='datetimepicker1'>								
+										<input type='text' class="form-control" name="dateTime" placeholder="Desired Departure Date" data-format="YYYY-MM-DD hh:mm:ss"/>
+										<span class="input-group-addon">
+											<span class="glyphicon glyphicon-calendar"></span>
+										</span>
+									</div>
 								</div>
 							</div>
 
