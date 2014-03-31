af006a798b2736764485a36ec2d572a8471ebbc5
sleewood <sleewood@purdue.edu>
2014-03-31 13:45:04 -0400

    testing php breaking

--- www/modules/editListings/editListingsProc.php
+++ www/modules/editListings/editListingsProc.php
@@ -64,47 +64,26 @@
 			}
 			else
 			{				
-				//THIS IS WHERE THE MAPQUEST API IS CALLED
-				//@author Evan Arnold <arnold4@purdue.edu>
-				//Sets the start/end lats and longs to their real-world values, and 0.0 if there is bad input (like just a state or gibberish)
-				//Start Location:
-				$lookupStartingAddress = str_replace(' ', '%20', $startingAddress);
-				$lookupDestinationAddress = str_replace(' ', '%20', $destinationAddress);
-				$mapquestResult = file_get_contents("http://www.mapquestapi.com/geocoding/v1/address?&key=Fmjtd%7Cluur210znh%2Cb0%3Do5-90ys0a&location=" . $lookupStartingAddress ."");
-				$parsedResult = json_decode($mapquestResult);
-				//Only parse result if the input address is good.
-				$addressQualityCode = $parsedResult->results->locations[0]->geocodeQualityCode;
-				if(qualityCodeCheck($addressQualityCode) === false)//If the quality code is bad
-				{
-					//GUYS WHAT DO WE DO IF IT IS BAD?
-					$startLatitude = 0.0;
-					$startLongitude = 0.0;
-				}				
-				else//If the quality of the input is good enough
-				{
-					$startLatitude = $parsedResult->results[0]->locations[0]->latLng->lat;//Add dat tab
-					$startLongitude = $parsedResult->results[0]->locations[0]->latLng->lng;//Add dat tab
-				}
+				$sql="UPDATE listings 
+				SET startingAddress = '$startingAddress', 
+				endingAddress = '$destinationAddress',
+				isRequest = '$isRequest',
+				passengers = '$passengers',
+				dateOfDeparture = '$dateTime'
+				WHERE listings_id = '$listings_id'";
 				
-				//EndLocation
-				$mapquestResult2 = file_get_contents("http://www.mapquestapi.com/geocoding/v1/address?&key=Fmjtd%7Cluur210znh%2Cb0%3Do5-90ys0a&location=" . $lookupDestinationAddress ."");
-				$parsedResult2 = json_decode($mapquestResult2);
-				//Only parse result if the input address is good.
-				$addressQualityCode2 = $parsedResult2->results->locations[0]->geocodeQualityCode;
-			//	if(qualityCodeCheck($addressQualityCode2) === false)//If the quality code is bad
-			//	{
-			//		//GUYS WHAT DO WE DO IF IT IS BAD?
-			//		$endLatitude = 0.0;
-			//		$endLongitude = 0.0;
-			//	}				
-			//	else//If the quality of the input is good enough
-			//	{
-				$endLatitude = $parsedResult2->results[0]->locations[0]->latLng->lat;//Add dat tab
-				$endLongitude = $parsedResult2->results[0]->locations[0]->latLng->lng;//Add dat tab
-			//	}
-
-				//At this point the start and end Latitudes and Longitudes /should/ be correct.... if there was bad input they are 0.0. We need to handle this.//This has been temporarily removed
-
+				if (!mysqli_query($con,$sql))
+				{
+					die('Error: ' . mysqli_error($con));
+					mysqli_close($con);
+				}
+				else
+				{
+					mysqli_close($con);
+					header('Location: ../../index.php');
+					exit();
+				}	
+			}			
 		?>
 
 	</body>
