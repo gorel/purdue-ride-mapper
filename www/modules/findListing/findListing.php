256b4d5773b903d239047c8fc7d0fc873d688e45
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-04-03 23:49:47 -0400

    Fixed findListing maps stuff

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -161,13 +161,15 @@
 											({
 												origin: [". $row['start_lat'] .", " . $row['start_long'] . "],
 												destination: [". $row['end_lat'].", " . $row['end_long'] . "],
+												travelMode: \'driving\',
+												strokeColor: \'#0000FF\',
 												strokeOpacity: 0.6,
 												strokeWeight: 6
 											});
 											map.addMarker
 											({
 												lat:". $row['end_lat'] . ",
-													lng:". $row['end_long'] . ",
+												lng:". $row['end_long'] . ",
 											})
 											map.fitZoom();
 										});
