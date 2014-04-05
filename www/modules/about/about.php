8ad04217aed1478f91fbdae68ec6bc12712ebd92
Evan Arnold <arnold44@purdue.edu>
2014-04-05 17:29:11 -0400

    Updated the about page text

--- www/modules/about/about.php
+++ www/modules/about/about.php
@@ -5,7 +5,7 @@
 		<h2 class="featurette-heading">Who is
 			<span class="text-muted">College Carpool?</span>
 		</h2>
-		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		<p class="lead">College Carpool is the product of a team of Computer Science students at Purdue University. It was created to replace the "Purdue Ride Share" Facebook page with a much easier to use website.</p>
 	</div>
 
 	<hr class="featurette-divider">
@@ -15,7 +15,7 @@
 		<h2 class="featurette-heading">Isabel Lee
 			<span class="text-muted">Team Leader, Developer</span>
 		</h2>
-		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		<p class="lead">Isabel is from Guayaquil, Ecuador and is a junior in Computer Science at Purdue University. Her role was to organize the team and incorporate Google Maps.</p>
 	</div>
 
 	<hr class="featurette-divider">
@@ -25,7 +25,7 @@
 		<h2 class="featurette-heading">Steven Leewood
 			<span class="text-muted">Developer</span>
 		</h2>
-		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		<p class="lead">Steven is from McCordsville, Indiana and is a junior in Computer Science at Purdue University. Steven’s job was to implement the front end design of the website and manage the ride listings.</p>
 	</div>
 
 	<hr class="featurette-divider">
@@ -35,7 +35,7 @@
 		<h2 class="featurette-heading">Logan Gore
 			<span class="text-muted">Developer</span>
 		</h2>
-		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		<p class="lead">Logan is a sophomore in Computer Science at Purdue from the small town of Pekin, IN. His job was to implement a matching algorithm to suggest potential riders and drivers.</p>
 	</div>
 	
 	<hr class="featurette-divider">
@@ -45,7 +45,7 @@
 		<h2 class="featurette-heading">Stephen Hong
 			<span class="text-muted">Developer</span>
 		</h2>
-		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		<p class="lead">Stephen is from Seoul, Korea and is a junior in Computer Science at Purdue. His role was to set up the server and help implement admin controls.</p>
 	</div>
 	
 	<hr class="featurette-divider">
@@ -55,7 +55,7 @@
 		<h2 class="featurette-heading">Timothy Thong
 			<span class="text-muted">Developer</span>
 		</h2>
-		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		<p class="lead">Timothy is from Singapore, and is a junior in Computer Science at Purdue. His role was to implement the email system and add administrative privileges.</p>
 	</div>
 	
 	<hr class="featurette-divider">
@@ -65,7 +65,7 @@
 		<h2 class="featurette-heading">Evan Arnold
 			<span class="text-muted">Developer</span>
 		</h2>
-		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		<p class="lead">Evan is in his sophomore year at Purdue, double majoring in Computer Science and Electrical Engineering Technology. He was in charge of ensuring appropriate website input and helped with the matching algorithm.</p>
 	</div>
 
 	<hr>
