11d0c85a26022a7566af786ee0bc6af81a4e7808
sleewood <sleewood@purdue.edu>
2014-04-16 18:36:10 -0400

    readded ajax for debugging

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -544,8 +544,7 @@
 			{
 				alert("Please enter a message!");
 				return;
-			}		
-			/*
+			}	
 			$.ajax ({
 				type: "POST",
 				url: "findListingContactProc.php",
@@ -567,7 +566,7 @@
 					alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
 					alert("responseText: "+xhr.responseText);
 				}
-			});*/
+			});
 		});
 
 		$('#routeModal').on('shown.bs.modal', function() {
