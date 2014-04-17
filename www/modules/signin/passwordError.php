9b766f7d6c7435af27039dc8d84809e8973611e0
tthong <tthong@purdue.edu>
2014-04-17 14:04:21 -0400

    Deleted sign-in error pages for AJAX implementation

--- www/modules/signin/passwordError.php
+++
@@ -1,6 +0,0 @@
-<?php
-	echo "Password does not match E-Mail address";
-	echo "You will be redirected to the main page in 5 seconds.. if you still use IE6 and just want to watch the world burn, refresh the page yourself";
-	sleep(5);
-	header("Location:../../index.php");
-?>
--- www/modules/signin/verifiedError.php
+++
@@ -1,6 +0,0 @@
-<?php
-	echo "Your account has not been verified";
-	echo "You will be redirected to the main page in 5 seconds.. if you still use IE6 and just want to watch the world burn, refresh the page yourself";
-	sleep(5);
-	header("Location:../../index.php");
-?>
