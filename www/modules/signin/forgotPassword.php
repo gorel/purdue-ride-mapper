f7b5018f0d393d1cc71255c1d62d397450e01ea9
tthong <timothy.tqm@gmail.com>
2014-04-19 23:00:34 -0400

    Randomized password reset hash

--- www/modules/signin/forgotPassword.php
+++ www/modules/signin/forgotPassword.php
@@ -3,7 +3,6 @@
 require '../../lib/hash.php';
 require '../../lib/email.php';
 
-//TODO Replace jscript alerts with friendly div elements
 
 //TODO CHANGE BACK
 $conn = new mysqli("collegecarpool.us", "root", "collegecarpool", "purdue_test");
@@ -14,9 +13,13 @@ if ($conn->connect_errno)
         die();
 }
 
-// Hash the email to produce the link to change the password
+// Hash email+time to produce the link to change the password
+
+date_default_timezone_set('EST');
 
 $email = $_POST['email'];
+$hash = saltedHash($email . date("H:i:s"));
+
 $query = "SELECT link from password_reset where email like ?";
 
 $stmt = $conn->stmt_init();
@@ -28,18 +31,24 @@ $stmt->store_result();
 
 if ($stmt->num_rows > 0) 
 {
+  
+  $query = "UPDATE password_reset set link=? where email =?";
+  $stmt = $conn->prepare($query);
+  $stmt->bind_param('ss', $hash, $email);
+  $stmt->execute();
+
   sendPwResetMail($email, $hash);		
+
   echo json_encode(array('retval' => 'OK'));
   return;
 }
-
-$hash = saltedHash($email);
 
 $query = "INSERT into password_reset (email, link) VALUES (?, ?)";
 $stmt = $conn->prepare($query);
 $stmt->bind_param('ss', $email, $hash);
 $stmt->execute();
 sendPwResetMail($email, $hash);
+
 echo json_encode(array('retval' => 'OK'));
 
 ?>
