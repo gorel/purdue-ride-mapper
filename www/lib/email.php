14190f0df64df9bc14806450285b7d56ed5a8b0e
tthong <timothy.tqm@gmail.com>
2014-04-22 01:26:08 -0400

    More fixes to request email

--- www/lib/email.php
+++ www/lib/email.php
@@ -125,10 +125,10 @@ function sendUserMail($rcpt, $rcpt_name, $msg, $requester, $requester_name, $req
 		
         $content = "<html><body>" .
                    "Hi $rcpt_name! <br><br> $requester_name has sent you " .
-                   "the following message: ----------<br><br> $msg <br><br> ." .
-                   "---------- <br><br> $requester has provided the following " .
+                   "the following message: <br><br> ---------- $msg <br><br> ." .
+                   "---------- <br><br> $requester_name has provided the following " .
                    "contact information for you to respond : <br><br> ". 
-                   " $requester_contact <br><br>" . "</body></html>";
+                   " $requester<br>$requester_contact <br><br>" . "</body></html>";
 				   
         mail($rcpt, $subject, $content, $headers);
 }
