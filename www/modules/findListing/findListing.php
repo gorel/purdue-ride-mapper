5128545a516a9201b993213094d3b9b6b494233d
sleewood <sleewood@purdue.edu>
2014-04-21 23:39:49 -0400

    trying to make findlistings pretty

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -92,29 +92,53 @@
 		<hr class="featurette-divider">
 		<div>
 			<h2 class="form-signin-heading">Search for a ride:</h2>
+			
+		<form class="form-inline" role="form">
+			<div class="form-group">
+				<label class="sr-only" for="exampleInputEmail2">Email address</label>
+				<input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
+			</div>
+			
+			<div class="form-group">
+				<label class="sr-only" for="exampleInputPassword2">Password</label>
+				<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+			</div>
+			<div class="checkbox">
+				<label>
+					<input type="checkbox"> Remember me
+				</label>
+			</div>
+			
+			<button type="submit" class="btn btn-default">Sign in</button>
+		</form>
+					
+			
+			
+			
+			
 			<form class="form-inline" role="form">
 				<div class="form-group">
-					<input id='starting_address_field' type="text" class="form-control" placeholder="Starting Address">
+					<input id="starting_address_field" type="text" class="form-control" placeholder="Starting Address">
 				</div>
 				
 				<div class="form-group">
-					<input id='ending_address_field' type="text" class="form-control" placeholder="Destination Address">
+					<input id="ending_address_field" type="text" class="form-control" placeholder="Destination Address">
 				</div>
 
 				<div class="form-group">
-					<input type='radio' name='mtype' value='requests'>Requests
-					<input type='radio' name='mtype' value='offers'>Offers
-					<input type='radio' name='mtype' value='both' checked>Both<br>
+					<input type="radio" name="mtype" value="requests">Requests
+					<input type="radio" name="mtype" value="offers">Offers
+					<input type="radio" name="mtype" value="both" checked>Both<br>
 				</div>
 				
 				<div class="form-group">
-					<div class='input-group date' id="datetimepicker2">
-						<input type='text' class="form-control" name="dateTime" placeholder="Desired Departure Date" data-format="YYYY-MM-DD hh:mm:ss"/>
+					<div class="form-group input-group date" id="datetimepicker2">
+						<input type="text" class="form-control" name="dateTime" placeholder="Desired Departure Date" data-format="YYYY-MM-DD hh:mm:ss"/>
 						<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
 					</div>
 				</div>
 				
-				<script type='text/javascript'>
+				<script type="text/javascript">
 					$(function () {
 						$('#datetimepicker2').datetimepicker();
 					});
@@ -170,7 +194,7 @@
 		</script>
 
 
-	<div id='matcher_wrapper'></div>
+	<div id="matcher_wrapper"></div>
 
 	<div>
 	<?php
