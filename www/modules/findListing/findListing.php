1407e9dac1ea303ef7943de77fd39006e8eabc75
sleewood <sleewood@purdue.edu>
2014-04-21 23:54:43 -0400

    trying to make findlistings pretty

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -91,7 +91,7 @@
 		<div id="map_canvas" style="height: 400px; width: 100%"></div>
 		<hr class="featurette-divider">
 		<div>
-			<h2 class="form-signin-heading">Search for a ride:</h2>			<!--	
+			<h2 class="form-signin-heading">Search for a ride:</h2>			
 			<form class="form-inline" role="form">
 				<div class="form-group">
 					<input id="starting_address_field" type="text" class="form-control" placeholder="Starting Address">
@@ -108,7 +108,7 @@
 				</div>
 				
 				<div class="form-group">
-					<div class="input-group date" id="datetimepicker2">
+					<div class="input-group date" id="datetimepicker2" width="20%">
 						<input type="text" class="form-control" name="dateTime" placeholder="Desired Departure Date" data-format="YYYY-MM-DD hh:mm:ss"/>
 						<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
 					</div>
@@ -120,8 +120,8 @@
 					});
 				</script>
 			</form>
-			<button class="btn btn-default" onclick="matchNewAddress(); return false;" >Search</button>				-->
-		<table class='table table-condensed'>
+			<button class="btn btn-default" onclick="matchNewAddress(); return false;" >Search</button>			
+		<!--<table class='table table-condensed'>
 			<thead>
 				<tr>
 					<th width="20%"> Starting Address </th>
@@ -130,7 +130,7 @@
 					<th width="20%"> Desired Departure Date </th>
 					<th width="20%"></th>
 				</tr>
-			</thead>";
+			</thead>
 			<tbody>
 				<tr>
 					<td><input id="starting_address_field" type="text" class="form-control" placeholder="Starting Address"></td>
@@ -155,7 +155,7 @@
 					<td><button class="btn btn-default" onclick="matchNewAddress(); return false;" >Search</button></td>
 				</tr>
 			</tbody>
-		</table>
+		</table>-->
 			
 		</div>
 		<br>
