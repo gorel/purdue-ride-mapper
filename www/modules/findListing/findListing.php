c3d647299f8cdf49e4dd8c292bf4dfa4e7d60871
sleewood <sleewood@purdue.edu>
2014-04-22 00:21:01 -0400

    displaying width

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -562,6 +562,10 @@
 			}
 			mysqli_close($con);
 		}
+		var tbl = document.getElementById('listingsTable');
+		var td = tbl.rows[0].cells[0];
+		var theWidth = td.width;
+		alert(theWidth);
 
 	?>
 	</div>
