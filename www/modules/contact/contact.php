9209618e10e6f48bfbee4eafd8e499a734389912
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-04-23 20:14:33 -0400

    Trying different table stuff

--- www/modules/contact/contact.php
+++ www/modules/contact/contact.php
@@ -35,6 +35,53 @@
 				<th> Reply </th>
 				<th> Delete </th>
 				</tr>
+				<tr>
+				<td> Estevan </td>
+				<td> Today! </td>
+				<td> button1 </td>
+				<td> button2 </td>
+				</tr>
+				<?php
+					session_start();
+					if(!$_SESSION['isAdmin'])
+					{
+						$cat   = $_POST["category"];
+						$msg   = $_POST["text"];
+						$user_id = $_SESSION['user_id'];
+
+						$dbName   = 'purdue_test';
+
+						// connect to local db
+
+						$conn =  new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+						if ($conn->connect_errn)
+						{
+						    echo  $conn->connect_errno . " " . $conn->connect_error;
+						    die;
+						}
+						$stmt = $conn->stmt_init();
+
+						// check if user exists
+						$query = "SELECT ticket_message FROM tickets WHERE user_id like $user_id";
+						$stmt = $conn->prepare($query);
+						$stmt->bind_param('s', $email);
+						$stmt->execute();
+						$stmt->store_result();
+
+						if ($stmt->num_rows > 0)
+						{
+						    echo  "User already exists";
+						    die;
+						}
+						else
+						{
+
+						}
+						$stmt->close();
+
+					}
+				?>
 			</table>
 		</div>
 	</div>
