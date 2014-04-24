a3371bca61103b198d809b2c69499b6e496b1325
m-isabel <m.isabellee@hotmail.com>
2014-04-23 20:19:03 -0400

    test

--- www/modules/findListing/findListing.php
+++ www/modules/findListing/findListing.php
@@ -409,7 +409,7 @@
 				}
 				else
 				{
-
+echo "<script> alert(\"ASDF\"); </script>";
 					$sqlCount = "SELECT COUNT(listings_id) FROM listings";
 					$countRes = mysqli_query($con,$sqlCount);
 					$rowCount = mysqli_fetch_row($countRes);
