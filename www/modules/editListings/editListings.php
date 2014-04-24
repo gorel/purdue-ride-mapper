e92a56f6eb90e5014d48873c3104e615ad1644ce
m-isabel <m.isabellee@hotmail.com>
2014-04-24 00:22:22 -0400

    Pagination for EditListing

--- www/modules/editListings/editListings.php
+++ www/modules/editListings/editListings.php
@@ -91,8 +91,45 @@
 			session_start();
 			$user_id = $_SESSION['user'];
 
-			$sql = "SELECT * FROM listings";
-			$result = mysqli_query($con,$sql);
+			$sqlCount = "SELECT COUNT(listings_id) FROM listings";
+					$countRes = mysqli_query($con,$sqlCount);
+					$rowCount = mysqli_fetch_row($countRes);
+					
+					//Total row count
+					$total = $rowCount[0];
+					
+					//Display this number of results
+					$page_rows = 5;
+					
+					//Keep track of previous page number
+					$last = ceil($total/$page_rows);
+					//echo "<script>console.log(\"".$last."\"); </script>";
+					// This makes sure $last cannot be less than 1
+					if($last < 1){
+						$last = 1;
+					}
+					// Establish the $pagenum variable
+					$pagenum = 1;
+					// Get pagenum from URL vars if it is present, else it is = 1
+					
+					if(isset($_GET['pn'])){
+						//echo "<script>console.log(\"ISSET\"); </script>";
+						$pagenum = preg_replace('#[^0-9]#', '', $_GET['pn']);
+					}
+					// This makes sure the page number isn't below 1, or more than our $last page
+					if ($pagenum < 1) { 
+						$pagenum = 1; 
+					} else if ($pagenum > $last) { 
+						$pagenum = $last; 
+					}
+					// This sets the range of rows to query for the chosen $pagenum
+					$limit = 'LIMIT ' .($pagenum - 1) * $page_rows .',' .$page_rows;
+					
+					$sql = "SELECT * FROM listings $limit";
+					$result = mysqli_query($con,$sql);
+//			$sql = "SELECT * FROM listings";
+//			$result = mysqli_query($con,$sql);
+
 			echo "<table class='table table-striped'>
 			<thead>
 			<tr>
@@ -103,6 +140,70 @@
 			<th> Date of Departure </th>
 			</tr>
 			</thead>";
+			// Establish the $paginationCtrls variable
+					$paginationCtrls = '';
+					//echo "<script>console.log(\"paginCtrls ".$paginationCtrls."\"); </script>";
+					// If there is more than 1 page worth of results
+					if($last != 1){
+						/* First we check if we are on page one. If we are then we don't need a link to 
+						   the previous page or the first page so we do nothing. If we aren't then we
+						   generate links to the first page, and to the previous page. */
+						if ($pagenum > 1) {
+						//echo "<script>console.log(\"pagenum ".$pagenum."\"); </script>";
+							$previous = $pagenum - 1;
+
+							//$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$previous.'">Previous</a> &nbsp; &nbsp; ';
+							$paginationCtrls .= '
+								<ul class="pagination">
+								  <li><a href="#" onclick="changePage('.$previous.');">Previous</a></li>
+								</ul>
+							';
+							//echo "<script>console.log(\"pagenum ".$previous."\"); </script>";
+							// Render clickable number links that should appear on the left of the target page number
+							for($i = $pagenum-4; $i < $pagenum; $i++){
+								if($i > 0){
+									//$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a> &nbsp; ';
+									$paginationCtrls .= '<ul class="pagination">
+									  <li><a href="#" onclick="changePage('.$i.');">'.$i.'</a></li>
+									</ul>
+									';
+									//echo "<script>console.log(\"pagenum1 ".$i."\"); </script>";
+								}
+							}
+						}
+						// Render the target page number, but without it being a link
+						//$paginationCtrls .= ''.$pagenum.' &nbsp; ';
+						$paginationCtrls .= '
+								<ul class="pagination">
+								  <li class="disabled"><a href="#" onclick="changePage('.$pagenum.');">'.$pagenum.'</a></li>
+								</ul>
+							';
+						//echo "<script>console.log(\"paginationCtrls ".$paginationCtrls."\"); </script>";
+						// Render clickable number links that should appear on the right of the target page number
+						for($i = $pagenum+1; $i <= $last; $i++){
+							//$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a> &nbsp; ';
+							//echo "<script>console.log(\"paginationCtrls a href ".$_SERVER['PHP_SELF']."?pn=".$i."\"); </script>";
+							//echo "<script>console.log(\"pagenum2 ".$i."\"); </script>";
+							$paginationCtrls .= '<ul class="pagination">
+									  <li><a href="#" onclick="changePage('.$i.');">'.$i.'</a></li>
+									</ul>
+									';
+							if($i >= $pagenum+4){
+								break;
+							}
+						}
+//						echo "<script>console.log(\"paginationCtrls a href \"); </script>";
+						// This does the same as above, only checking if we are on the last page, and then generating the "Next"
+						if ($pagenum != $last) {
+							$next = $pagenum + 1;
+							//$paginationCtrls .= ' &nbsp; &nbsp; <a href="'.$_SERVER['PHP_SELF'].'?pn='.$next.'">Next</a> ';
+							//echo "<script>console.log(\"pagenum3 ".$next."\"); </script>";
+							$paginationCtrls .= '<ul class="pagination">
+									  <li><a href="#" onclick="changePage('.$i.');">Next</a></li>
+									</ul>
+									';
+						}
+					}
 			while($row = mysqli_fetch_array($result))
 			{
 				if($_SESSION['user'] == $row["user_id"] || $_SESSION['isAdmin']==1)
