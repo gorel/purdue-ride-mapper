79d842ef5a5ded460a49d71434d9a175c87ada0e
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-04-25 13:48:27 -0400

    fixed quotation issue

--- www/modules/contact/contact.php
+++ www/modules/contact/contact.php
@@ -161,8 +161,7 @@ function deleteTicket(ticketID)
 									}
 									echo "<td> " , $row['ticket_date'] , "</td>";
 									echo "<td> Reply </td>";
-									$resolveQuery = "UPDATE tickets SET resolved = 1 WHERE ticket_id = $ticket_id"
-									echo "<td> <button type=\"button\" onclick=\"deleteTicket(".$ticket_id.")\">Delete</button> </td>";
+									echo '<td> <button type="button" onclick="deleteTicket('.$ticket_id.')">Delete</button> </td>';
 									echo "</tr>";
 									echo "<tr id=\"". $row['$ticket_id']."_Body2>";
 									echo "<td colspan=\"4\"> " , $row['ticket_message'] , "</td>";
