6dda88abc24dec04a579268d0f4be9503d1a4dbc
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-04-27 16:43:48 -0400

    break the code!

--- www/modules/contact/replyTicketProc.php
+++ www/modules/contact/replyTicketProc.php
@@ -1,7 +1,7 @@
 <?php
-	$ticket_id = $_POST["ticket_id"];
-	$ticket_answer = $_POST["ticket_answer"];
-	$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
+	//$ticket_id = $_POST["ticket_id"];
+	//$ticket_answer = $_POST["ticket_answer"];
+	//$con=mysqli_connect("localhost","collegecarpool","collegecarpool","purdue_test");
 
 	echo json_encode(array('success' => "asdfasdf"));
 	return;
