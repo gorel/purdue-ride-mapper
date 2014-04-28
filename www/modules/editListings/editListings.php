6e45fd9a9fe1c704afbfb57b94d692ec3e3a5af8
Isabel Lee <m.isabellee@hotmail.com>
2014-04-27 23:34:50 -0400

    Implementing previous fix

--- www/modules/editListings/editListings.php
+++ www/modules/editListings/editListings.php
@@ -153,7 +153,15 @@
 			//$sql = "SELECT * FROM listings $limit";
 			echo "<script>console.log(\"".$limit."\"); </script>";
 			
-			$sql = "SELECT * FROM listings WHERE user_id =$user_id $limit";
+			if(!$_SESSION['isAdmin'])
+			{
+				$sql = "SELECT * FROM listings WHERE user_id =$user_id $limit";
+			}
+			else
+			{
+				$sql = "SELECT * FROM listings $limit";
+			}
+
 			$result = mysqli_query($con,$sql);
 			
 			echo "<table class='table table-striped'>
