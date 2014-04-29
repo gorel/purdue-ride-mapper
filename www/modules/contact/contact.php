cd7c74e8390e0f0875abfab07b526aafaaa21ac5
mrsakkaro <mrsakkaro@dream-farmer.com>
2014-04-29 00:18:27 -0400

    done some stuff

--- www/modules/contact/contact.php
+++ www/modules/contact/contact.php
@@ -129,7 +129,17 @@ session_start();
 		</div>
 		<div class="col-md-4">
 			<form enctype="application/x-www-form-urlencoded" class="form-horizontal" action="/modules/contact/contactCreateTicketProc.php" method="POST"><div class="padded">
-				<h2 class="form-signin-heading">Create Ticket</h2>
+				<h2 class="form-signin-heading">Contact Us</h2>
+				<div class="control-group"><label for="category" class="control-label required">Category</label>
+					<div class="controls">
+						<select name="category" id="category" class="form-control">
+						<option value="0">Making a Listing</option>
+						<option value="1">Finding a Listing</option>
+						<option value="2">Other</option>
+						</select>
+					</div>
+				</div>
+
 				<div class="control-group"><label for="text" class="control-label required">Message</label>
 					<div class="controls">
 						<textarea name="text" id="text" rows="6" class="form-control" cols="80"></textarea>
